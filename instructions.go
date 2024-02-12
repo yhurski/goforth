e321a8a91e3e8d22fa03645e686f3aead2ce56fa
Yury Hurski <hi1sailor@gmail.com>
2024-02-12 04:33:49 +0300

    Add executePrimitive, ', execute and .s words

--- instructions.go
+++ instructions.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 var lastPrimitiveId = I_NOOP
 
 const (
@@ -10,8 +14,36 @@ const (
 	I_DIV
 	I_NEG
 
+	I_TICK
+	I_EXECUTE
+
+	I_DOTS
+
 	I_NOOP // must always be last
 )
+
+func executePrimitive(execToken int) {
+	switch execToken {
+	case I_EXIT:
+		exitOp()
+	case I_PLUS:
+		plusOp()
+	case I_MINUS:
+		minusOp()
+	case I_MULT:
+		multOp()
+	case I_DIV:
+		divOp()
+	case I_NEG:
+		negateOp()
+	case I_TICK:
+		executeOp()
+	case I_EXECUTE:
+
+	case I_DOTS:
+		dotsOp()
+	}
+}
 
 func exitOp() {
 
@@ -44,4 +76,23 @@ func divOp() {
 func negateOp() {
 	operand := dataStack.Pop()
 	dataStack.push(-operand)
+}
+
+func tickOp() {
+	// get next word from buffer
+	// search for the word in the dict
+	// push the word' xt onto stack
+}
+
+func executeOp() {
+	execToken := dataStack.Pop()
+	executePrimitive(execToken)
+}
+
+// func executeByToken(xt int) {
+
+// }
+
+func dotsOp() {
+	fmt.Printf("S[dataStack.len()]:%v\n", dataStack)
 }
