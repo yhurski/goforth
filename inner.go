ab5fe2466324e3874cb8219704ddbd998325b19d
Yury Hurski <hi1sailor@gmail.com>
2024-02-17 17:12:37 +0300

    Add 'word not found!' message to compilation mode

--- inner.go
+++ inner.go
@@ -73,7 +73,7 @@ func findOrCompile(word string) {
 		} else {
 			number, err := strconv.Atoi(word)
 			if err != nil {
-				fmt.Println(err)
+				fmt.Println("word not found!")
 				return
 			}
 
