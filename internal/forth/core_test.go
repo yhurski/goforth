c9e1a2913aca51aee424e50b58a421cb348ce2ac
Yury Hurski <hi1sailor@gmail.com>
2024-03-12 13:37:01 +0300

    Add basic table test to core_test.go

--- internal/forth/core_test.go
+++ internal/forth/core_test.go
@@ -1,6 +1,27 @@
 package forth
 
-import "testing"
+import (
+	"testing"
+)
+
+func TestInitialCoreValues(t *testing.T) {
+	var test = []struct {
+		message      string
+		variable     any
+		initValue    any
+		errorMessage string
+	}{
+		{"state should be 0", state, 0, "Initial state is not interpeting (0): %d"},
+	}
+
+	for _, input := range test {
+		t.Run(input.message, func(t *testing.T) {
+			if input.variable != input.initValue {
+				t.Errorf(input.errorMessage, input.variable)
+			}
+		})
+	}
+}
 
 func TestInitialState(t *testing.T) {
 	if state != 0 {
