192a682f7f2622d59d81adee2d16f050b3d608f9
Yury Hurski <hi1sailor@gmail.com>
2024-03-12 23:00:18 +0300

    Replace tests in core_test.go with single table test

--- internal/forth/core_test.go
+++ internal/forth/core_test.go
@@ -12,6 +12,9 @@ func TestInitialCoreValues(t *testing.T) {
 		errorMessage string
 	}{
 		{"state should be 0", state, 0, "Initial state is not interpeting (0): %d"},
+		{"ip should be 0", ip, uint32(0), "Initial ip is not 0: %d"},
+		{"pIn should be 0", pIn, 0, "Initial pIn is not 0: %d"},
+		{"errorCode should be 0", errorCode, byte(0), "Initial errorCode is not 0: %d"},
 	}
 
 	for _, input := range test {
@@ -20,29 +23,5 @@ func TestInitialCoreValues(t *testing.T) {
 				t.Errorf(input.errorMessage, input.variable)
 			}
 		})
-	}
-}
-
-func TestInitialState(t *testing.T) {
-	if state != 0 {
-		t.Errorf("Initial state is not interpeting (0): %d", state)
-	}
-}
-
-func TestInitialIp(t *testing.T) {
-	if ip != 0 {
-		t.Errorf("Initial ip is not 0: %d", ip)
-	}
-}
-
-func TestInitialpIn(t *testing.T) {
-	if pIn != 0 {
-		t.Errorf("Initial pInt is not 0: %d", pIn)
-	}
-}
-
-func TestInitialErrorCode(t *testing.T) {
-	if pIn != 0 {
-		t.Errorf("Initial errorCode is not 0: %d", errorCode)
 	}
 }
